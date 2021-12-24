df2431ba0fe6c8dd66cf6a88185f4157d04a1194
Will Hodges <hodgeswt@users.noreply.github.com>
2021-12-24 03:14:22 -0500

    all the widgets

--- BlockedAds/BlockedAds.swift
+++ BlockedAds/BlockedAds.swift
@@ -4,11 +4,22 @@
 //
 //  Created by Will Hodges on 12/24/21.
 //
-
+import Foundation
 import WidgetKit
 import SwiftUI
 import Intents
 import SwiftHole
+
+
+func round(_ value: Double, toNearest: Double) -> Double {
+    return round(value / toNearest) * toNearest
+}
+
+extension Double {
+    func truncate(places : Int)-> Double {
+        return Double(floor(pow(10.0, Double(places)) * self)/pow(10.0, Double(places)))
+    }
+}
 
 enum DefaultsFetchError: Error {
     case emptyDefaults
@@ -16,7 +27,10 @@ enum DefaultsFetchError: Error {
 }
 
 struct DataRetriever {
-    static func fetch(completion: @escaping (Result<Int,Error>) -> ()) {
+    static func fetch(type: WidgetEnum, completion: @escaping (Result<String,Error>) -> ()) {
+        
+        let index = type.rawValue
+        
         let defaults = UserDefaults(suiteName: "group.com.will-hodges.Pi-Hole-Manager")
         let ip = defaults!.string(forKey: "ip") ?? ""
         let apiKey = defaults!.string(forKey: "apiKey") ?? ""
@@ -28,7 +42,22 @@ struct DataRetriever {
             hole.fetchSummary() { result in
                 switch result {
                 case .success(let data):
-                    completion(.success(data.adsBlockedToday))
+                    switch index {
+                    case 1:
+                        completion(.success(String(data.dnsQueriesToday)))
+                    case 2:
+                        completion(.success(String(round(data.adsPercentageToday, toNearest: 0.1).truncate(places: 2)) + "%"))
+                    case 3:
+                        completion(.success(String(data.domainsBeingBlocked)))
+                    case 4:
+                        completion(.success(String(data.uniqueClients)))
+                    case 5:
+                        completion(.success(String(data.queriesCached)))
+                    case 6:
+                        completion(.success(String(data.adsBlockedToday)))
+                    default:
+                        completion(.success(String(data.adsBlockedToday)))
+                    }
                 case .failure(_):
                     completion(.failure(DefaultsFetchError.emptyApiResponse))
                 }
@@ -40,29 +69,55 @@ struct DataRetriever {
 struct Provider: IntentTimelineProvider {
     
     func placeholder(in context: Context) -> BlockedAdsEntry {
-        BlockedAdsEntry(date: Date(), adsBlocked: 100)
+        BlockedAdsEntry(date: Date(), text: "Ads Blocked", data: "100", color: .blue)
     }
 
     func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (BlockedAdsEntry) -> ()) {
-        let entry = BlockedAdsEntry(date: Date(), adsBlocked: 100)
+        let entry = BlockedAdsEntry(date: Date(), text: "Ads Blocked", data: "100", color: .blue)
         completion(entry)
     }
 
     func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        
-        DataRetriever.fetch() { result in
+        DataRetriever.fetch(type: configuration.PiHoleWidgetType) { result in
             let currentDate = Date()
             let refreshDate: Date
             
             var entry: BlockedAdsEntry
             var entries: [BlockedAdsEntry] = [BlockedAdsEntry]()
             
+            var text: String
+            var color: SwiftUI.Color
+            
+            switch configuration.PiHoleWidgetType.rawValue {
+            case 1:
+                text = "Queries"
+                color = .green
+            case 2:
+                text = "Percentage Blocked"
+                color = .orange
+            case 3:
+                text = "Blocklist"
+                color = .red
+            case 4:
+                text = "Clients"
+                color = .cyan
+            case 5:
+                text = "Cache Size"
+                color = .indigo
+            case 6:
+                text = "Blocked"
+                color = .blue
+            default:
+                text = "Blocked"
+                color = .blue
+            }
+            
             switch result {
             case .success(let blocked):
-                entry = BlockedAdsEntry(date: currentDate, adsBlocked: blocked)
+                entry = BlockedAdsEntry(date: currentDate, text: text, data: blocked, color: color)
                 refreshDate = Calendar.current.date(byAdding: .minute, value: 5, to: currentDate)!
             case .failure(_):
-                entry = BlockedAdsEntry(date: currentDate, adsBlocked: 0)
+                entry = BlockedAdsEntry(date: currentDate, text: text, data: "0", color: color)
                 refreshDate = Calendar.current.date(byAdding: .minute, value: 1, to: currentDate)!
             }
             entries.append(entry)
@@ -75,7 +130,9 @@ struct Provider: IntentTimelineProvider {
 
 struct BlockedAdsEntry: TimelineEntry {
     let date: Date
-    let adsBlocked: Int
+    let text: String
+    let data: String
+    let color: SwiftUI.Color
 }
 
 struct BlockedAdsEntryView : View {
@@ -85,11 +142,11 @@ struct BlockedAdsEntryView : View {
     var body: some View {
         ZStack {
             VStack {
-                Text("Ads Blocked")
+                Text(entry.text)
                     .font(.body)
                 Spacer()
                     .fixedSize(horizontal: false, vertical: true)
-                Text(String(entry.adsBlocked))
+                Text(String(entry.data))
                     .font(.title)
                     .foregroundColor(.white)
             }
@@ -106,10 +163,10 @@ struct BlockedAds: Widget {
         IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
             BlockedAdsEntryView(entry: entry)
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .background(SwiftUI.Color.blue)
+                .background(entry.color)
         }
-        .configurationDisplayName("Blocked Ads")
-        .description("This widget shows the number of ads your PiHole has blocked today.")
+        .configurationDisplayName("PiHole Widget")
+        .description("This widget shows data from your PiHole.")
     }
     
 }
